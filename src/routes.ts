08ad5319d3f5f6a22a4a2590d01496cc85651868
Mike Haydon <mike.haydon@stayfrontdesk.com>
2023-08-23 09:31:21 -0500

    Added route to routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,37 +1,41 @@
-
-import { Router, Request, Response, NextFunction } from 'express';
-import { createHmac } from 'crypto';
-import * as os from 'os';
-import { callbackHostname, frontSecret, randomString } from './server';
+import { Router, Request, Response, NextFunction } from "express";
+import { createHmac } from "crypto";
+import * as os from "os";
+import { callbackHostname, frontSecret, randomString } from "./server";
+import { FrontConnector, InboundMessageRequest } from "./front_connector";
 
 const ChannelRouter = Router();
 
 // FRONT ROUTES
-ChannelRouter.use('/front', verifyFrontRequest);
+ChannelRouter.use("/front", verifyFrontRequest);
 
 /**
  * This route is called when a user in Front creates a new instance of this channel type.
  * We are expected to respond with a type of 'success' as well as a webhook URL that tells
  * Front where to send message sync updates about this channel.
- * 
+ *
  * In this implementation, we respond with /front/${randomString(16)}.
  */
-ChannelRouter.post('/', async (req: Request, res: Response) => {
-  if (req.body.type !== 'authorization') {
-    res.send(400).json({ type: 'bad_request', message: 'Unknown type sent to channel' });
+ChannelRouter.post("/", async (req: Request, res: Response) => {
+  if (req.body.type !== "authorization") {
+    res
+      .send(400)
+      .json({ type: "bad_request", message: "Unknown type sent to channel" });
   }
 
   const webhookHostname = callbackHostname || os.hostname();
   const webhookId = randomString(16);
   const webhookUrl = `${webhookHostname}/front/${webhookId}`;
-  console.log(`Creating webhook with URL ${webhookUrl} for channel with ID ${req.body.payload.channel_id}`);
- /**
-  * If you want to sync messages from the external channel into Front, store the channel_id from 
-  * req.body.payload.channel_id and maintain its value for use when you call the Channel API in the front_connector.ts file.
-  */
+  console.log(
+    `Creating webhook with URL ${webhookUrl} for channel with ID ${req.body.payload.channel_id}`
+  );
+  /**
+   * If you want to sync messages from the external channel into Front, store the channel_id from
+   * req.body.payload.channel_id and maintain its value for use when you call the Channel API in the front_connector.ts file.
+   */
 
   return res.status(200).json({
-    type: 'success',
+    type: "success",
     webhook_url: webhookUrl,
   });
 });
@@ -39,21 +43,28 @@ ChannelRouter.post('/', async (req: Request, res: Response) => {
 /**
  * This route is called whenever a message is sent from Front (either a new message _or_ an autoreply) OR
  * when a message was successfully imported and processed by Front.
- * 
+ *
  * We are expected to respond with an external_id which identifies the message as well as an external_conversation_id
  * which tells Front which conversation to thread that message into. Each conversation in Front contains a list of
  * conversation_ids which identify it. If we respond with _any_ of those IDs as the external_conversation_id, the message
  * will be threaded into that conversation.
- * 
- * Note that, in this implementation, we generate random external_ids and external_conversation_ids. 
+ *
+ * Note that, in this implementation, we generate random external_ids and external_conversation_ids.
  */
-ChannelRouter.post('/front/:webhookId', async (req: Request, res: Response) => {
-  if (!['message', 'message_autoreply', 'message_imported'].includes(req.body.type)) {
-    return res.send(400).json({ type: 'bad_request', message: 'Unknown message type sent to channel' });
+ChannelRouter.post("/front/:webhookId", async (req: Request, res: Response) => {
+  if (
+    !["message", "message_autoreply", "message_imported"].includes(
+      req.body.type
+    )
+  ) {
+    return res.send(400).json({
+      type: "bad_request",
+      message: "Unknown message type sent to channel",
+    });
   }
 
-  if (req.body.type === 'message_imported') {
-    console.log('Message import notification received from Front');
+  if (req.body.type === "message_imported") {
+    console.log("Message import notification received from Front");
     return res.status(200).json({});
   }
 
@@ -62,7 +73,7 @@ ChannelRouter.post('/front/:webhookId', async (req: Request, res: Response) => {
   const external_conversation_id = randomString(16);
 
   return res.status(200).json({
-    type: 'success',
+    type: "success",
     external_id,
     external_conversation_id,
   });
@@ -71,58 +82,93 @@ ChannelRouter.post('/front/:webhookId', async (req: Request, res: Response) => {
 /**
  * This route is called by Front whenever an instance of a channel is deleted by a user.
  */
-ChannelRouter.delete('/front/:webhookId', async (req: Request, res: Response) => {
-  if (req.body.type !== 'delete') {
-    res.send(400).json({ type: 'bad_request', message: 'Unknown delete type sent to channel' });
-  }
+ChannelRouter.delete(
+  "/front/:webhookId",
+  async (req: Request, res: Response) => {
+    if (req.body.type !== "delete") {
+      res.send(400).json({
+        type: "bad_request",
+        message: "Unknown delete type sent to channel",
+      });
+    }
 
-  console.log(`Channel with ID ${req.body.payload.channel_id} was deleted`);
-  return res.sendStatus(200);
-});
+    console.log(`Channel with ID ${req.body.payload.channel_id} was deleted`);
+    return res.sendStatus(200);
+  }
+);
 
 // OAUTH ROUTES
 
 /**
  * If our channel is configured using OAuth, this route will be called by Front whenever a new channel
  * is instantiated in order to obtain an authorization code that can be exchanged for an access token in the future.
- * 
+ *
  * Note that this channel does not properly implement OAuth 2.0, and instead just responds with random credentials
  * for the sake of simplicity.
  */
-ChannelRouter.get('/oauth/authorize', async (req: Request, res: Response) => {
+ChannelRouter.get("/oauth/authorize", async (req: Request, res: Response) => {
   const frontRedirectUri = new URL(req.query.redirect_uri as string);
   const code = randomString(16);
-  frontRedirectUri.searchParams.append('code', code);
-  frontRedirectUri.searchParams.append('state', req.query.state as string);
+  frontRedirectUri.searchParams.append("code", code);
+  frontRedirectUri.searchParams.append("state", req.query.state as string);
 
-  console.log('Returning authorization code to Front');
+  console.log("Returning authorization code to Front");
   return res.status(301).redirect(frontRedirectUri.toString());
 });
 
-/** 
+/**
  * Once Front has obtained an authorization code for our channel, it will call this endpoint to exchanged that
  * code for an access token (and, optionally, a refresh token).
- * 
+ *
  * Note that this channel does not properly implement OAuth 2.0, and instead just responds with random credentials
- * for the sake of simplicity. 
+ * for the sake of simplicity.
  */
-ChannelRouter.post('/oauth/token', async (req: Request, res: Response) => {
-  console.log('Returning access token to Front');
-  return res.status(200).json({ access_token: randomString(32), refresh_token: randomString(32) });
+ChannelRouter.post("/oauth/token", async (req: Request, res: Response) => {
+  console.log("Returning access token to Front");
+  return res
+    .status(200)
+    .json({ access_token: randomString(32), refresh_token: randomString(32) });
+});
+
+ChannelRouter.get("/importMessages", async (req: Request, res: Response) => {
+  console.log("Importing messages from Front");
+
+  const payload: InboundMessageRequest = {
+    sender: {
+      handle: "ex do nisi",
+      name: "occaecat nulla eu proident",
+    },
+    body: "minim mollit enim aliqua",
+    metadata: {
+      external_id: "non quis",
+      external_conversation_id: "commodo voluptate et velit",
+    },
+    subject: "nisi non dolore in",
+    delivered_at: 95629829,
+  };
+
+  console.log("cheetos");
+
+  await FrontConnector.importInboundMessage("cha_dviqc", payload);
+
+  // console.log(response);
+  return res.status(200).json({ type: "success" });
 });
 
 // HELPER FUNCTIONS
 function verifyFrontRequest(req: Request, res: Response, next: NextFunction) {
-  const timestamp = req.headers['x-front-request-timestamp'];
+  const timestamp = req.headers["x-front-request-timestamp"];
   const rawBody = JSON.stringify(req.body);
   const baseString = `${timestamp}:${rawBody}`;
 
-  const hmac = createHmac('sha256', frontSecret)
+  const hmac = createHmac("sha256", frontSecret)
     .update(baseString)
-    .digest('base64');
-    
-  if (hmac !== req.headers['x-front-signature']) {
-    return res.send(400).json({ type: 'bad_request', message: 'Signature not verified' });
+    .digest("base64");
+
+  if (hmac !== req.headers["x-front-signature"]) {
+    return res
+      .send(400)
+      .json({ type: "bad_request", message: "Signature not verified" });
   }
 
   next();
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
-import express from 'express';
-import ChannelRouter from './routes';
-import { randomBytes } from 'crypto';
-import bodyParser from 'body-parser';
+import express from "express";
+import ChannelRouter from "./routes";
+import { randomBytes } from "crypto";
+import bodyParser from "body-parser";
 
-export const frontId = 'yourFrontId';
-export const frontSecret = 'shhhhhhhhh';
-export const frontUrl = 'https://api2.frontapp.com/';
-export const callbackHostname = 'https://your-ngrok-hostnmae.ngrok.io';
-export const serverPort = '3000';
+export const frontId = "4eef3112a1700ef0ceb0";
+export const frontSecret = "1a6f0efaed17c3dae4884248b31b4e7ccc1c0724";
+export const frontUrl = "https://api2.frontapp.com/";
+export const callbackHostname = "https://e30c-96-37-102-208.ngrok-free.app/";
+export const serverPort = "3000";
 
 export function randomString(length: number): string {
-  return randomBytes(Math.floor(length / 2)).toString('hex');
+  return randomBytes(Math.floor(length / 2)).toString("hex");
 }
 
 const app = express();
